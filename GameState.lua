e69c9f6907e8b760cbc4c763d33e6fbff9cd5068
bobsayshilol <bobsayshilol@live.co.uk>
2024-02-17 17:33:09 +0000

    Add physics

--- GameState.lua
+++ GameState.lua
@@ -1,24 +1,29 @@
 local class = {}
 
 local MoveSpeed = 30
-local IslandSpawnTime = 3
+local IslandSpawnTime = 5
+local MaxBridgeLength = 20
 
 local Island
 local Worker
 local IslandGraph
 
+local function vec_len2(vec)
+	return vec.x * vec.x + vec.y * vec.y
+end
+
 local function update_camera(camera, dt)
 	if love.keyboard.isDown("w", "up") then
-		camera.pos_y = camera.pos_y - MoveSpeed * dt
+		camera.pos_y = camera.pos_y - MoveSpeed * dt / (camera.scale_y / 10)
 	end
 	if love.keyboard.isDown("s", "down") then
-		camera.pos_y = camera.pos_y + MoveSpeed * dt
+		camera.pos_y = camera.pos_y + MoveSpeed * dt / (camera.scale_y / 10)
 	end
 	if love.keyboard.isDown("a", "left") then
-		camera.pos_x = camera.pos_x - MoveSpeed * dt
+		camera.pos_x = camera.pos_x - MoveSpeed * dt / (camera.scale_x / 10)
 	end
 	if love.keyboard.isDown("d", "right") then
-		camera.pos_x = camera.pos_x + MoveSpeed * dt
+		camera.pos_x = camera.pos_x + MoveSpeed * dt / (camera.scale_x / 10)
 	end
 
 	-- Clamp it
@@ -30,7 +35,8 @@ local function update_camera(camera, dt)
 end
 
 local function spawn_island(self)
-	self.islands[#self.islands + 1] = Island.new(100, 0)
+	local island = Island.new(300, 0, self.world)
+	table.insert(self.islands, island)
 end
 
 function class.load()
@@ -53,37 +59,53 @@ function class.new()
 			return { x = self.scale_x * (pos.x - self.pos_x), y = self.scale_y * (pos.y - self.pos_y) }
 		end,
 		from_screen = function(self, pos)
-			return { x = pos.x / self.scale_x + self.pos_x, y = pos.y / self.scale_y - self.pos_y }
+			return { x = pos.x / self.scale_x + self.pos_x, y = pos.y / self.scale_y + self.pos_y }
 		end
 	}
 
 	state.next_island_time = 0
 
+	local BridgeStates = { Idle = 0, Started = 1 }
+	state.current_bridge_state = BridgeStates.Idle
+	state.bridge_start = nil
+	state.bridge_island = nil
+
 	state.enter = function(self)
+		-- Setup physics
+		self.world = love.physics.newWorld()
+
+		-- Create the initial island
 		local center = self.camera:from_screen({
 			x = love.graphics.getPixelWidth() / 2,
 			y = love.graphics.getPixelHeight() / 2
 		})
-		state.islands[1] = Island.new(center.x, center.y, Island.Shapes.StartingIsland)
+		state.islands[1] = Island.new(center.x, center.y, self.world, true)
 		state.workers[1] = Worker.new(center.x, center.y)
 
 		state.next_island_time = IslandSpawnTime
+
+		spawn_island(self)
 	end
 
 	state.update = function(self, dt)
 		-- Movement
 		update_camera(self.camera, dt)
+		self.world:update(dt)
+		-- Update all physics
+		for _, island in pairs(self.islands) do
+			island:update(dt)
+		end
+
+		-- Update worker logic
+		for _, worker in pairs(self.workers) do
+			worker:update(dt)
+		end
 
 		-- Spawn any new islands
 		self.next_island_time = self.next_island_time - dt
 		if self.next_island_time < 0 then
 			self.next_island_time = IslandSpawnTime
 			spawn_island(self)
-		end
-
-		-- Logic
-		for _, worker in pairs(self.workers) do
-			worker:update(dt)
 		end
 
 		return nil
@@ -103,6 +125,18 @@ function class.new()
 		for _, worker in pairs(self.workers) do
 			worker:draw(self.camera)
 		end
+		if state.bridge_start ~= nil then
+			local start_s = self.camera:to_screen(state.bridge_start)
+			local stop_s = { x = love.mouse.getX(), y = love.mouse.getY() }
+			local stop_w = self.camera:from_screen(stop_s)
+			local delta = { x = stop_w.x - state.bridge_start.x, y = stop_w.y - state.bridge_start.y }
+			if vec_len2(delta) > MaxBridgeLength * MaxBridgeLength then
+				love.graphics.setColor(1, 0, 0)
+			else
+				love.graphics.setColor(0, 0, 1)
+			end
+			love.graphics.line(start_s.x, start_s.y, stop_s.x, stop_s.y)
+		end
 	end
 
 	state.exit = function(self)
@@ -112,6 +146,41 @@ function class.new()
 
 	state.keypressed = function(self, key)
 		--print(key)
+	end
+
+	state.mousepressed = function(self, x, y, button)
+		local pos = self.camera:from_screen({ x = x, y = y })
+		local try_hit = function()
+			local hit = nil
+			self.world:queryBoundingBox(pos.x, pos.y, pos.x, pos.y,
+				function(fixture)
+					-- Just because the AABB overlaps doesn't mean that it's a hit
+					if fixture:testPoint(pos.x, pos.y) then
+						hit = fixture:getUserData()
+						return false
+					end
+					return true
+				end)
+			-- TODO: mustn't be on the "main" land bit
+			return hit
+		end
+
+		if state.current_bridge_state == BridgeStates.Idle then
+			local hit = try_hit()
+			if hit ~= nil then
+				state.bridge_start = pos
+				state.bridge_island = hit
+				state.current_bridge_state = BridgeStates.Started
+			end
+		elseif state.current_bridge_state == BridgeStates.Started then
+			local hit = try_hit()
+			if hit ~= nil and hit ~= state.bridge_island then
+				print("TODO: join islands")
+			end
+			state.bridge_start = nil
+			state.bridge_island = nil
+			state.current_bridge_state = BridgeStates.Idle
+		end
 	end
 
 	state.wheelmoved = function(self, x, y)
--- Island.lua
+++ Island.lua
@@ -4,25 +4,49 @@ local TileSize = 5
 
 class.Shapes = {
     StartingIsland = {
-        { x = 0, y = 0 },
-        { x = 0, y = 1 },
-        { x = 0, y = 2 },
-        { x = 1, y = 0 },
-        { x = 1, y = 1 },
-        { x = 1, y = 2 },
-        { x = 2, y = 0 },
-        { x = 2, y = 1 },
-        { x = 2, y = 2 },
+        graphics = {
+            { x = 0, y = 0 },
+            { x = 0, y = 1 },
+            { x = 0, y = 2 },
+            { x = 1, y = 0 },
+            { x = 1, y = 1 },
+            { x = 1, y = 2 },
+            { x = 2, y = 0 },
+            { x = 2, y = 1 },
+            { x = 2, y = 2 },
+        },
+        physics = {
+            0 * TileSize, 0 * TileSize,
+            3 * TileSize, 0 * TileSize,
+            3 * TileSize, 3 * TileSize,
+            0 * TileSize, 3 * TileSize,
+        }
     },
     Basic = {
-        { x = 0, y = 0 },
-        { x = 1, y = 0 },
+        graphics = {
+            { x = 0, y = 0 },
+            { x = 1, y = 0 },
+        },
+        physics = {
+            0 * TileSize, 0 * TileSize,
+            2 * TileSize, 0 * TileSize,
+            2 * TileSize, 1 * TileSize,
+            0 * TileSize, 1 * TileSize,
+        }
     },
     Square = {
-        { x = 0, y = 0 },
-        { x = 0, y = 1 },
-        { x = 1, y = 0 },
-        { x = 1, y = 1 },
+        graphics = {
+            { x = 0, y = 0 },
+            { x = 0, y = 1 },
+            { x = 1, y = 0 },
+            { x = 1, y = 1 },
+        },
+        physics = {
+            0 * TileSize, 0 * TileSize,
+            2 * TileSize, 0 * TileSize,
+            2 * TileSize, 2 * TileSize,
+            0 * TileSize, 2 * TileSize,
+        }
     },
 }
 
@@ -42,17 +66,28 @@ local function random_shape()
     end
 end
 
-function class.new(x, y, shape)
+function class.new(x, y, world, start)
     local state = {}
     state.position = { x = x, y = y }
-    state.shape = shape or random_shape()
+    state.shape = start and class.Shapes.StartingIsland or random_shape()
+
+    -- Physics
+    state.body = love.physics.newBody(world, x, y, "dynamic")
+    local shape2d = love.physics.newPolygonShape(state.shape.physics)
+    state.fixture = love.physics.newFixture(state.body, shape2d)
+    state.fixture:setUserData(state)
+
+    -- All newly spawned islands move to the left
+    if not start then
+        state.body:setLinearVelocity(-5, 0)
+    end
 
     state.draw = function(self, camera)
         local tile_size = {
             x = TileSize * camera.scale_x,
             y = TileSize * camera.scale_y,
         }
-        for _, tile_pos in pairs(self.shape) do
+        for _, tile_pos in pairs(self.shape.graphics) do
             local origin = camera:to_screen({
                 x = self.position.x + TileSize * tile_pos.x,
                 y = self.position.y + TileSize * tile_pos.y,
@@ -60,6 +95,11 @@ function class.new(x, y, shape)
             love.graphics.setColor(0, 0.7, 0)
             love.graphics.rectangle("fill", origin.x, origin.y, tile_size.x, tile_size.y)
         end
+    end
+
+    state.update = function(self, dt)
+        state.position.x = state.body:getX()
+        state.position.y = state.body:getY()
     end
 
     return state
